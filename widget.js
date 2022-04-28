2ea7793b271da54c94714bdac39e2cb418daabf4
Stephen Shi <stephen.shi@carsales.com.au>
2022-04-28 11:24:47 +1000

    font and more words

--- widget.js
+++ widget.js
@@ -19,10 +19,225 @@ for (let i = 0; i < alphabet.length; i++) {
 }
 
 // game state
+const Dictionary = [
+	"Abuse",
+	"Adult",
+	"Agent",
+	"Anger",
+	"Apple",
+	"Award",
+	"Basis",
+	"Beach",
+	"Birth",
+	"Block",
+	"Blood",
+	"Board",
+	"Brain",
+	"Bread",
+	"Break",
+	"Brown",
+	"Buyer",
+	"Cause",
+	"Chain",
+	"Chair",
+	"Chest",
+	"Chief",
+	"Child",
+	"China",
+	"Claim",
+	"Class",
+	"Clock",
+	"Coach",
+	"Coast",
+	"Court",
+	"Cover",
+	"Cream",
+	"Crime",
+	"Cross",
+	"Crowd",
+	"Crown",
+	"Cycle",
+	"Dance",
+	"Death",
+	"Depth",
+	"Doubt",
+	"Draft",
+	"Drama",
+	"Dream",
+	"Dress",
+	"Drink",
+	"Drive",
+	"Earth",
+	"Enemy",
+	"Entry",
+	"Faith",
+	"Fault",
+	"Field",
+	"Fight",
+	"Final",
+	"Floor",
+	"Focus",
+	"Force",
+	"Frame",
+	"Frank",
+	"Front",
+	"Fruit",
+	"Glass",
+	"Grant",
+	"Grass",
+	"Green",
+	"Group",
+	"Guide",
+	"Heart",
+	"Henry",
+	"Horse",
+	"Hotel",
+	"House",
+	"Index",
+	"Input",
+	"Issue",
+	"Japan",
+	"Jones",
+	"Judge",
+	"Knife",
+	"Laura",
+	"Layer",
+	"Level",
+	"Lewis",
+	"Light",
+	"Limit",
+	"Lunch",
+	"Major",
+	"March",
+	"Match",
+	"Metal",
+	"Model",
+	"Money",
+	"Month",
+	"Motor",
+	"Mouth",
+	"Music",
+	"Night",
+	// Noise,
+	// North,
+	// Novel,
+	// Nurse,
+	// Offer,
+	// Order,
+	// Other,
+	// Owner,
+	// Panel,
+	// Paper,
+	// Party,
+	// Peace,
+	// Peter,
+	// Phase,
+	// Phone,
+	// Piece,
+	// Pilot,
+	// Pitch,
+	// Place,
+	// Plane,
+	// Plant,
+	// Plate,
+	// Point,
+	// Pound,
+	// Power,
+	// Press,
+	// Price,
+	// Pride,
+	// Prize,
+	// Proof,
+	// Queen,
+	// Radio,
+	// Ratio,
+	// Reply,
+	// Right,
+	// River,
+	// Round,
+	// Route,
+	// Rugby,
+	// Scale,
+	// Scene,
+	// Scope,
+	// Score,
+	// Sense,
+	// Shape,
+	// Share,
+	// Sheep,
+	// Sheet,
+	// Shift,
+	// Shirt,
+	// Shock,
+	// Sight,
+	// Simon,
+	// Skill,
+	// Sleep,
+	// Smile,
+	// Smith,
+	// Smoke,
+	// Sound,
+	// South,
+	// Space,
+	// Speed,
+	// Spite,
+	// Sport,
+	// Squad,
+	// Staff,
+	// Stage,
+	// Start,
+	// State,
+	// Steam,
+	// Steel,
+	// Stock,
+	// Stone,
+	// Store,
+	// Study,
+	// Stuff,
+	// Style,
+	// Sugar,
+	// Table,
+	// Taste,
+	// Terry,
+	// Theme,
+	// Thing,
+	// Title,
+	// Total,
+	// Tower,
+	// Track,
+	// Trade,
+	// Train,
+	// Trend,
+	// Trial,
+	// Trust,
+	// Truth,
+	// Uncle,
+	// Union,
+	// Unity,
+	// Value,
+	// Video,
+	// Visit,
+	// Voice,
+	// Waste,
+	// Watch,
+	// Water,
+	// While,
+	// White,
+	// Whole,
+	// Woman,
+	// World,
+	// Youth,
+];
+
+const getRandomElement = arr => {
+	if (arr.length === 0) return;
+	return arr[Math.floor(Math.random() * arr.length)];
+};
 const word = [];
 const words = [];
 let nextCellId = 0;
-const correctWordStr = "apple".toUpperCase();
+const correctWordStr = getRandomElement(Dictionary).toUpperCase();
+console.log(correctWordStr);
 
 const isSingleLetter = str => {
 	if (str.length !== 1) return false;
@@ -90,9 +305,9 @@ const handleKeyDown = e => {
 		});
 
 		// whether win
-		if (correctWordStr === word.join("")) {
-			alert("win");
-		}
+		// if (correctWordStr === word.join("")) {
+		// 	alert("win");
+		// }
 
 		word.splice(0, word.length);
 		words.push([...word]);
@@ -102,40 +317,3 @@ const handleKeyDown = e => {
 document.addEventListener("keydown", e => {
 	handleKeyDown(e);
 });
-
-// Irrelevant to the game
-// get word from row and column directions
-const correctArr = ["q1w1e1", "w0w1w2w3"];
-const arr2d = [
-	["q0", "w0"],
-	["q1", "w1", "e1"],
-	["q2", "w2", "e2"],
-	["q3", "w3"],
-];
-
-const wordArr = []; //string
-let maxLength = -Infinity;
-for (let y = 0; y < arr2d.length; y++) {
-	wordArr.push(arr2d[y].join(""));
-	if (arr2d.length > maxLength) {
-		maxLength = arr2d.length;
-	}
-}
-
-for (let x = 0; x < maxLength; x++) {
-	let str = "";
-	for (let y = 0; y < arr2d.length; y++) {
-		str += arr2d[y][x] ? arr2d[y][x] : "";
-	}
-	wordArr.push(str);
-}
-
-const wordArrFiltered = wordArr.filter(str => {
-	return str !== "";
-});
-
-console.log(
-	wordArrFiltered.filter(str => {
-		return correctArr.includes(str);
-	})
-);
